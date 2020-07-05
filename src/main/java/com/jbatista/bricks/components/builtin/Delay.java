ed0997a4450ca86cd9162188dadc924a2a8dafcf
j.batistareis <j.batistareis@gmail.com>
2020-07-05 08:50:13 -0300

    added step variable

--- src/main/java/com/jbatista/bricks/components/builtin/Delay.java
+++ src/main/java/com/jbatista/bricks/components/builtin/Delay.java
@@ -11,16 +11,16 @@ public class Delay extends FilterModule {
     public Delay() {
         name = "Delay";
 
-        filter =  new Comb();
+        filter = new Comb();
 
         controllers.add(new Controller(
                 "Duration", "Sets the duration for the effect",
-                0.01, 1, 0.01, Controller.Curve.ORIGINAL,
+                0.01, 1, 0.01, 0.01, Controller.Curve.ORIGINAL,
                 comb::setDuration));
 
         controllers.add(new Controller(
                 "Lines", "Sets amount of delay lines",
-                1, 20, 1, Controller.Curve.ORIGINAL,
+                1, 20, 1, 1, Controller.Curve.ORIGINAL,
                 comb::setTaps,
                 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20));
     }
